46de6e6943d966dd555a01e9b4d7833e9d3955bc
Micutio <wagner.mchl@googlemail.com>
2017-07-07 15:55:08 +0800

    Fixed: error where installation folder was not created sometimes

--- 2_install_dist.sh
+++ 2_install_dist.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "-> unpacking distribution"
+mkdir inst
 tar -xf dist/ComplexAutomatonBase-0.9.tar.gz --directory inst/
 cd inst/ComplexAutomatonBase-0.9
 echo "-> installing test distribution"
