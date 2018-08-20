9252d38211c56d7e4bd5e22614550452ff655ee9
Michael Wagner <wagner.mchl@googlemail.com>
2018-08-21 01:40:29 +0800

    Updated: uninstall script

--- 3_uninst_dist_solus.sh
+++ 3_uninst_dist_solus.sh
@@ -2,5 +2,5 @@
 
 # TODO: detect the python version and installation location automatically!
 
-sudo rm -R /usr/lib/python3.5/site-packages/cab
-sudo rm -R /usr/lib/python3.5/site-packages/ComplexAutomatonBase-0.9-py3.5.egg-info
+sudo rm -R /usr/lib/python3.6/site-packages/cab
+sudo rm -R /usr/lib/python3.6/site-packages/ComplexAutomatonBase-0.9-py3.6.egg-info
