38eb0f3f1871d6217217f2f6685eff7d3cfd39b0
Michael Wagner <wagner.mchl@googlemail.com>
2018-08-22 16:29:04 +0800

    Added: debug output to cab sys

--- cab/cab_system.py
+++ cab/cab_system.py
@@ -3,12 +3,7 @@ ComplexAutomaton module of the Complex Automaton Base.
 Contains the automaton itself.
 """
 
-# External library imports.
-# import pygame
-# import math
-# from pygame.locals import *
 
-# Internal Simulation System component imports.
 from cab.cab_global_constants import GlobalConstants
 from cab.abm.cab_abm import ABM
 from cab.ca.cab_ca_rect import CARect
@@ -16,8 +11,8 @@ from cab.ca.cab_ca_hex import CAHex
 
 from cab.util.cab_io_tk import TkIO
 from cab.util.cab_io_pygame import PygameIO
-
 from cab.util.cab_rng import seed_RNG
+from util.cab_logging import *
 
 __author__ = 'Michael Wagner'
 
@@ -37,6 +32,7 @@ class ComplexAutomaton:
         seed_RNG(self.gc.RNG_SEED)
 
         if 'proto_agent' in kwargs:
+            debug('[ComplexAutomaton] have proto agent {0}'.format(kwargs['proto_agent']))
             self.abm = ABM(self.gc, proto_agent=kwargs['proto_agent'])
             self.proto_agent = kwargs['proto_agent']
         else:
@@ -44,21 +40,28 @@ class ComplexAutomaton:
             self.proto_agent = None
 
         if 'proto_cell' in kwargs:
+            debug('[ComplexAutomaton] have proto agent {0}'.format(kwargs['proto_agent']))
             if self.gc.USE_HEX_CA:
+                debug('[ComplexAutomaton] initializing hexagonal CA')
                 self.ca = CAHex(self, proto_cell=kwargs['proto_cell'])
             else:
+                debug('[ComplexAutomaton] initializing rectangular CA')
                 self.ca = CARect(self, proto_cell=kwargs['proto_cell'])
             self.proto_cell = kwargs['proto_cell']
         else:
             if self.gc.USE_HEX_CA:
+                debug('[ComplexAutomaton] initializing hexagonal CA')
                 self.ca = CAHex(self)
             else:
+                debug('[ComplexAutomaton] initializing rectangular CA')
                 self.ca = CARect(self)
             self.proto_cell = None
 
         if self.gc.gui == "TK":
+            debug('[ComplexAutomaton] initializing Tk IO')
             self.visualizer = TkIO(self.gc, self)
         elif self.gc.gui == "PyGame":
+            debug('[ComplexAutomaton] initializing Pygame IO')
             self.visualizer = PygameIO(self.gc, self)
         self.display_info()
 
@@ -71,7 +74,7 @@ class ComplexAutomaton:
               "\n ".format(self.gc.TITLE, self.gc.VERSION))
 
     def reset_simulation(self):
-        print('resetting simulation')
+        info('resetting simulation')
         self.abm.__init__(self.gc, proto_agent=self.proto_agent)
         self.ca.__init__(self, proto_cell=self.proto_cell)
         self.gc.TIME_STEP = 0
