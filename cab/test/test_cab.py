50804494e423312c4166b9da2fea59f049e3338a
Michael Wagner <wagner.mchl@googlemail.com>
2018-11-26 00:11:40 +0800

    Changed: Enabled verbose logging during test

--- cab/test/test_cab.py
+++ cab/test/test_cab.py
@@ -1,6 +1,7 @@
 # CAB libraries
 from cab.complex_automaton import ComplexAutomaton
 from cab.global_constants import GlobalConstants
+import cab.util.logging as cab_log
 
 # External libraries
 import unittest
@@ -12,6 +13,7 @@ class GeneralTestCase(unittest.TestCase):
     """
 
     def test_startup(self):
+        cab_log.set_log_trace()
         gc = GlobalConstants()
         simulation = ComplexAutomaton(gc)
         self.assertTrue(simulation)
